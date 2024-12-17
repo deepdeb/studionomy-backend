748effe9763c8061ade1e4369ff691c7ba8cface
deepdeb <debnath.pradeep619@gmail.com>
2024-12-17 16:42:01 +0530

    modify, deleted & created files

--- src/api/controllers/freelancer/freelancerRequest.js
+++ src/api/controllers/freelancer/freelancerRequest.js
@@ -1,30 +1,30 @@
 const logger = require("../../../config/logger");
 const Joi = require('joi');
-const freelancerPaymentService = require('../../services/freelancerPaymentService');
-exports.getFreelancerPaymentController = async (req, res) => {
+const freelancerRequestService = require('../../services/freelancerRequestService');
+exports.getFreelancerRequestController = async (req, res) => {
     try {
-        const freelancerPaymentDataCheck = Joi.object({
+        const freelancerRequestDataCheck = Joi.object({
             userId: Joi.number().required(),
             userType: Joi.number().required(),
         })
 
         // Validate the data
-        const { error, value } = freelancerPaymentDataCheck.validate(req.body);
+        const { error, value } = freelancerRequestDataCheck.validate(req.body);
         if (error) {
-            logger.error(`Invalid freelancer payment data: ${error.details[0].message}`);
+            logger.error(`Invalid freelancer Request data: ${error.details[0].message}`);
             return res.status(400).json({ success: false, message: error.details[0].message.replace(/["':]/g, '') });
         }
-        logger.info(`Valid freelancer payment data`);
+        logger.info(`Valid freelancer Request data`);
 
-        const resp = await freelancerPaymentService.getFreelancerPayment(req.body);
+        const resp = await freelancerRequestService.getFreelancerRequest(req.body);
         if (resp) {
             return res.json({ success: true, status: 200, message: "", response: resp[0], totalCount: resp[1] });
         } else {
             return res.json({ success: false, status: 500, message: "Internal server error", response: [] });
         }
     } catch (error) {
-        logger.info('freelancer payment controller error: ', error);
-        console.log('freelancer payment controller error: ', error);
+        logger.info('freelancer Request controller error: ', error);
+        console.log('freelancer Request controller error: ', error);
         return res.json({ success: false, status: 400, message: res.message, response: [] });
     }
 }
--- src/api/routes/freelancerRoute.js
+++ src/api/routes/freelancerRoute.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 const { freelancerController } = require('../controllers/freelancer/freelancer');
 const { freelancerlistController } = require('../controllers/freelancer/freelancerlist');
-const { getFreelancerPaymentController } = require("../controllers/freelancer/freelancerPayment");
+const { getFreelancerRequestController } = require("../controllers/freelancer/freelancerRequest");
 const { submitPaymentController } = require("../controllers/freelancer/submitPayment");
 
 const authenticateToken = require('../middlewares/authenticateTokenUser');
 
 router.post("/freelancerSubmit", authenticateToken, freelancerController);
 router.post("/freelancerList", authenticateToken, freelancerlistController);
-router.post("/freelancerPayment", authenticateToken, getFreelancerPaymentController);
+router.post("/freelancerRequest", authenticateToken, getFreelancerRequestController);
 router.post("/submitPayment", submitPaymentController);
 module.exports = router;
