d10d20081db498b098f0086d754a0b0fbd3276e1
deepdeb <debnath.pradeep619@gmail.com>
2024-12-18 14:17:33 +0530

    modify files

--- src/api/services/freelancerEOPaymentByJobService.js
+++ src/api/services/freelancerEOPaymentByJobService.js
@@ -3,7 +3,7 @@ const logger = require("../../config/logger");
 
 exports.getFreelancerEOPaymentByJob = async (data) => {
     try {
-        let sql = "SELECT fl_eo_payment_id, req_id, job_id, job_number, payment_to, payment_to_userType, payment_from, payment_from_userType, payment_amount, credit_amount, DATE_FORMAT(payment_date, '%Y-%m-%d %H:%i %p') as payment_date, payment_status FROM fl_eo_payment_details WHERE payment_to = ? AND payment_to_userType = ? AND req_id = ?"
+        let sql = "SELECT fl_eo_payment_id, req_id, job_id, job_number, payment_to, payment_to_userType, payment_from, payment_from_userType, payment_amount, credit_amount, DATE_FORMAT(payment_date, '%Y-%m-%d %H:%i %p') as payment_date, reject_remarks, payment_status FROM fl_eo_payment_details WHERE payment_to = ? AND payment_to_userType = ? AND req_id = ?"
 
         const [resp] = await readPool.query(sql, [data.userId, data.userType, data.req_id]);
 
