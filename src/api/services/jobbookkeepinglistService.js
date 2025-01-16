1d14abfa02398abd9031f7d9c6182970b9b149f3
deepdeb <debnath.pradeep619@gmail.com>
2025-01-16 13:19:03 +0530

    modify files

--- src/api/services/jobbookkeepinglistService.js
+++ src/api/services/jobbookkeepinglistService.js
@@ -4,7 +4,7 @@ exports.getAllJobBookkeepingList = async (data) => {
     try {
         let searchCondition = '';
         if(data.payment_criteria) {
-            searchCondition += " AND (jbk.job_number LIKE '%" + data.payment_criteria + "%' OR j.cust_phoneNo LIKE '%" + data.payment_criteria + "%')"
+            searchCondition += " AND (jbk.job_number LIKE '%" + data.payment_criteria + "%' OR j.cust_phoneNo LIKE '%" + data.payment_criteria + "%' OR jbk.payment_description LIKE '%" + data.payment_criteria + "%' OR jbk.job_value LIKE '%" + data.payment_criteria + "%' OR jbk.job_details LIKE '%" + data.payment_criteria + "%' OR jbk.payment_type LIKE '%" + data.payment_criteria + "%' OR jbk.payment_status LIKE '%" + data.payment_criteria + "%')"
         }
         if (data.startDate && data.endDate) {
             let sql = "SELECT jbk.jb_id,jbk.payment_date,jbk.job_id,jbk.job_number,jbk.job_value,jbk.job_details,jbk.payment_description,jbk.payment_amount,jbk.payment_type,jbk.payment_status,jbk.profit,j.cust_phoneNo FROM job_book_keeping jbk LEFT JOIN jobs j ON jbk.userId = j.userId WHERE jbk.isDeleted = 0 AND jbk.userId = " + data.userId + " AND jbk.userType = " + data.userType + " AND jbk.job_number = j.job_number AND jbk.job_id = j.job_id AND jbk.payment_date BETWEEN '" + data.startDate + "' AND '" + data.endDate + "'" + searchCondition + " ORDER BY jbk.payment_date desc LIMIT ? OFFSET ?"
@@ -27,7 +27,8 @@ exports.getAllJobBookkeepingList = async (data) => {
         return resp2;
     }
     catch (err) {
-        logger.error(err);
+        logger.error('get all bookkeeping list controller error:', err);
+        console.log('get all bookkeeping list controller error:', err);
         return false;
     }
 }
